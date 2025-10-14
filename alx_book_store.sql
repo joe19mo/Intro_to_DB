aea88b9d0884f7d95aa0542b4cca3aad13430813
yousef mohamed <ymobos8@gmail.com>
2025-10-14 13:28:26 +0300

    book store fixed version

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,42 +1,29 @@
--- Create the database
 CREATE DATABASE alx_book_store;
-
--- Use the database
 USE alx_book_store;
-
--- Create Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+    author_name VARCHAR(215)
 );
-
--- Create Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+    title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-
--- Create Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
-
--- Create Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
--- Create Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,

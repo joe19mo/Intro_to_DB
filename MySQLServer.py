cd45de38c4abd8bcffef36385c5cacf91f032b83
yousef mohamed <ymobos8@gmail.com>
2025-10-18 21:15:07 +0300

    my sql server to create database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,13 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
     try:
         # محاولة الاتصال بسيرفر MySQL
         connection = mysql.connector.connect(
-            host='localhost',      # أو 127.0.0.1
-            user='root',           # غيّرها لو اسم المستخدم مختلف
-            password='1332006'  # ← حط كلمة المرور بتاعتك هنا
+            host='localhost',
+            user='root',           # غيّر لو اسم المستخدم مختلف
+            password='1332006'  # ← حط باسوردك هنا
         )
 
         if connection.is_connected():
@@ -16,12 +15,11 @@ def create_database():
             # إنشاء قاعدة البيانات (لو مش موجودة)
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    
-    except Error as e:
+
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        # إغلاق الاتصال مهما حصل
         if connection and connection.is_connected():
             cursor.close()
             connection.close()
@@ -29,3 +27,4 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
